ea5de79a15c8354d57cccce73ceb38396f7afdb5
Manuel Wenner <hi@manuelwenner.de>
2025-10-20 11:04:23 +0200

    feat: step 2- add fullscreen image and overlap it with text

--- app/src/main/java/de/manuelwenner/moviejunkie/MainActivity.kt
+++ app/src/main/java/de/manuelwenner/moviejunkie/MainActivity.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import de.manuelwenner.moviejunkie.ui.theme.MovieJunkieTheme
 
@@ -26,20 +35,47 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                 ) { innerPadding ->
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxWidth(),
-                    ) {
-                        Headline()
-                        Welcome(
-                            name = "Manuel",
-                        )
-                    }
+                    HomeScreen(innerPadding)
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun HomeScreen(innerPadding: PaddingValues) {
+    val image = painterResource(R.drawable.movie)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+        ) {
+            Headline()
+            Welcome(
+                name = "Manuel",
+            )
+
+            Button(
+                onClick = {},
+                modifier = Modifier.padding(top = 32.dp)
+            ) {
+                Text("Bild wechseln")
+            }
+        }
+
     }
 }
 
@@ -61,17 +97,7 @@ fun Welcome(name: String) {
 fun GreetingPreview() {
     MovieJunkieTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxWidth()
-            ) {
-                Headline()
-                Welcome(
-                    name = "Manuel"
-                )
-            }
+            HomeScreen(innerPadding)
         }
     }
 }
